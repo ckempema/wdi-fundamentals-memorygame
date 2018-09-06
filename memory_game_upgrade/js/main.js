3396bc73cc567b65aaf47c1b5cca59514ea6a6df
Calder Kempema <calder.kempema.1@us.af.mil>
2018-09-06 15:17:43 -0400

    updates to number of cards

--- memory_game_upgrade/js/main.js
+++ memory_game_upgrade/js/main.js
@@ -1,58 +1,59 @@
 //Back end for memory game per WDI Fundamentals prework
 
-var cards = [       //Create 4 card objects
-    {
-        //cardId: 1
-        rank: "queen",
-        suit: "hearts",
-        cardImage: "images/queen-of-hearts.png"
-    },
-    {
-        //cardId: 2
-        rank: "queen",
-        suit: "diamonds",
-        cardImage: "images/queen-of-diamonds.png"
-    },
-    {
-        //cardId: 3
-        rank: "king",
-        suit: "hearts",
-        cardImage: "images/king-of-hearts.png",
-    },
-    {
-        //cardId: 4
-        rank: "king",
-        suit: "diamonds",
-        cardImage: "images/king-of-diamonds.png"
-    }
-];
+var numCards = 16;       //the total number of cards on the game board
 var cardsInPlay = [];
 var cardsFound = [];
+var matchesFound = 0;
+var movesMade = 0;
 
-var flipCard = function(){
-    cardId = this.getAttribute('data-id');
+function Card(rank, suit, type){    //Constructor for a Card Object
+    this.rank = rank;
+    this.suit = suit;
+    this.remaining = 2;
+
+    if(type === "color"){                                   //determines if the card is gray or color, should be set to one of those two
+        this.cardImage = "images/"+rank+"-of-"+suit+"-color.png"; //set the file path for colored cards
+    }
+    else {                                                  //set the file path for gray cards
+        this.cardImage = "images/"+rank+"-of-"+suit+"-gray.png"
+    } 
+}
+
+var cards = [                       //Create 8 different card objects
+    new Card("queen", "hearts", "color"),
+    new Card("queen", "diamonds", "color"),
+    new Card("queen", "hearts", "gray"),
+    new Card("queen", "diamonds", "gray"),
+    new Card("king", "hearts", "color"),
+    new Card("king", "diamonds", "color"),
+    new Card("king", "hearts", "gray"),
+    new Card("king", "hearts", "gray")
+];
+
+
+var flipCard = function(){          // Flip a card if it gets clicked on, check for matches
+    cardType = this.getAttribute('type-id');
 
     if(this.getAttribute('flip-status') === 'back' && cardsInPlay.length < 2){ //flip the card face up
-        this.setAttribute('src', cards[cardId].cardImage);
+        this.setAttribute('src', cards[cardType].cardImage);
         this.setAttribute('flip-status', 'flipped')
-        cardsInPlay.push(cardId);
+        cardsInPlay.push(cardType);
 
-        if(cardsInPlay.length === 2){ //check if two cards in play match
-            if(cards[cardsInPlay[0]].rank === cards[cardsInPlay[1]].rank){
+        if(cardsInPlay.length === 2){                       //check if two cards in play match
+            if(cardsInPlay[0] === cardsInPlay[1]){
                 console.log("You found a match!");
-                this.setAttribute()
             }
         }
     }
     else if (this.getAttribute('flip-status')==='flipped'){ //reverse a card and remove it from play
         this.setAttribute('src', 'images/back.png');        //reset the card image
         this.setAttribute('flip-status', 'back');           //chage cards status
-        removalIndex = cardsInPlay.indexOf(cardId);         //find card to remove in cardsInPlay
+        removalIndex = cardsInPlay.indexOf(cardType);         //find card to remove in cardsInPlay
         if(removalIndex >= 0){                              //ensure a valid index was found
             cardsInPlay.splice(removalIndex, 1);            //remove index of card
         }
         else{                                               //Otherwise print an error message
-            console.log("Error removing cardId " + cardId + "index not found")
+            console.log("Error removing cardId " + cardType + "index not found")
         }
     }
     else{
@@ -60,19 +61,41 @@ var flipCard = function(){
     }    
 }
 
-var createBoard = function(){
-    for(i = 0; i < cards.length; i++) {
-        var cardElement = document.createElement('img');
-        cardElement.setAttribute('src', 'images/back.png');
-        cardElement.setAttribute('data-id', i);
-        cardElement.setAttribute('flip-status', 'back');
+var setupBoard = function(){
+    for(i=0; i<numCards;i++){
+        cardElement = document.getElementById("card-"+i);
+        cardElement.setAttribute('type-id', selectCardType(0));
+        cardElement.setAttribute('flip-status','back');
         cardElement.addEventListener('click', flipCard);
-        document.getElementById('game-board').appendChild(cardElement);
     }
 }
 
-createBoard();
+var selectCardType = function(level){               //recursive routine to select a random but available cardtype
+    seed = Math.floor(Math.random()*8)
+    if(cards[seed].remaining > 0){                  //ensure there are not too many cards of that type already on board
+        cards[seed].remaining -= 1;
+        return seed;
+    }
+    else if (level >= 100){                         //Arbitrary exit limit to prevent endless recurision
+        console.log('Error: Not enough cards');
+        return 0;
+    }
+    else {                                          //find a new seed recursively 
+        return selectCardType(level+1);
+    }
+}
 
+var showBoard = function(){                         //Shows the faces of all cards on the game board
+    for(i=0;i<numCards;i++){
+        cardElement = document.getElementById("card-"+i);
+        cardType = cardElement.getAttribute("type-id");
+        cardElement.setAttribute("src", cards[cardType].cardImage);
+        cardElement.setAttribute("flip-status","flipped");
+    }
+}
+
+setupBoard();
+showBoard();
 
 
 
